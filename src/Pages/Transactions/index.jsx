9e8134673f731b3ddcc3ddfb8f267a922cb6403c
Merci Jacob <mercihabam@gmail.com>
2021-09-13 11:58:13 +0200

    add paginations

--- src/Pages/Transactions/index.jsx
+++ src/Pages/Transactions/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react';
 import { ScrollView } from 'react-native';
+import { DataTable } from 'react-native-paper';
 import { useDispatch, useSelector } from 'react-redux';
 import { getTransactions } from '../../Redux/actions/transactions';
 import { agentId } from '../../Utils/helpers';
@@ -12,17 +13,29 @@ function ViewTransactions({navigation}){
     const [ activeDate, setActive ] = useState('week');
     const { data } = useSelector(({ users: { currUser } }) =>currUser);
     const { dataSt } = useSelector(({ stations: {currStation} }) =>currStation);
+    const { countTrans } = useSelector(({ transactions: {transactions} }) =>transactions);
     const dispatch = useDispatch();
     const rKey = navigation.getState().history[0].key
+    const [offset, setOffset] = useState(0);
+    const [currentPage, setCurrent] = useState(0);
+    const limit = 10;
 
     useEffect(() =>{
-        getTransactions(dataSt.id, agentId(data.Agents, dataSt.id), 0, 20, type)(dispatch)
-    }, [data, dataSt, type, rKey])
+        getTransactions(dataSt.id, agentId(data.Agents, dataSt.id), offset, limit, type)(dispatch)
+    }, [data, dataSt, type, rKey, offset, limit, currentPage])
 
     return(
         <ScrollView>
             <TransHeader type={type} setType={setType} active={activeDate} setActive={setActive} />
             <TransData />
+            <DataTable.Pagination
+                page={currentPage}
+                numberOfPages={Math.ceil(countTrans / limit)+1}
+
+                onPageChange={page => {setCurrent(page); setOffset((page-1)*limit)}}
+                numberOfItemsPerPage={limit}
+                selectPageDropdownLabel={'Rows per page'}
+            />
         </ScrollView>
     )
 };
